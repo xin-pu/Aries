1db68634f93c2d844164b3ddcc96f7d40bdd4250
xin-pu <pu.xin@outlook.com>
2021-05-08 16:13:32 +0800

    Initialize the UI

---
+++ src/Aries/AriesMain.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using HandyControl.Tools;
+
+namespace Aries
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+
+}
