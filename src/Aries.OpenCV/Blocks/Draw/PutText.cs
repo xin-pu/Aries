75cbc3e77876e50cfdcd6a76b68ede8ccaf4ac96
xin-pu <pu.xin@outlook.com>
2021-06-02 16:31:25 +0800

    Complete Puttext block.

--- src/Aries.OpenCV/Blocks/Draw/PutText.cs
+++ src/Aries.OpenCV/Blocks/Draw/PutText.cs
@@ -1,19 +1,39 @@
 ﻿using System.ComponentModel;
+using System.Linq;
 using Aries.OpenCV.GraphModel;
+using OpenCvSharp;
 
 namespace Aries.OpenCV.Blocks
 {
     [Category("Draw")]
     public class PutText : ProcessingBlock
     {
+        [Category("IN_MAT")] public TextSegment[] Texts { set; get; }
+        [Category("Enter")] public double Color { set; get; } = 255;
+
+        [Category("Enter")] public int Thickness { set; get; } = 1;
+
+        [Category("Enter")] public LineTypes LineType { set; get; } = LineTypes.Link8;
+
+        [Category("Enter")] public HersheyFonts HersheyFont { set; get; } = HersheyFonts.HersheySimplex;
+        [Category("Enter")] public double FontScale { set; get; } = 11;
+
+        [Category("Enter")] public bool BottomLeftOrign { set; get; } = false;
+
         public override bool CanExecute()
         {
-            throw new System.NotImplementedException();
+            return InPutMat != null && Texts != null && Texts.Length > 0;
         }
 
         public override void Execute()
         {
-            throw new System.NotImplementedException();
+            OutPutMat = new Mat();
+            InPutMat.CopyTo(OutPutMat);
+            Texts.ToList().ForEach(text =>
+            {
+                Cv2.PutText(OutPutMat, text.Text, text.Point, HersheyFont, FontScale, new Scalar(Color), Thickness,
+                    LineType, BottomLeftOrign);
+            });
         }
     }
 }
---
+++ src/Aries.OpenCV/Blocks/Import/CreateTexts.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Aries.OpenCV.GraphModel;
+using OpenCvSharp;
+
+namespace Aries.OpenCV.Blocks
+{
+    [Category("Initial")]
+    public class CreateTexts : ImportBlock<TextSegment[]>
+    {
+        [Category("Enter")] public string List_Texts { set; get; }
+
+        private List<string[]> textArray { set; get; }
+
+        public override bool CanExecute()
+        {
+            var point = List_Texts
+                .Split(new[] {";"}, StringSplitOptions.RemoveEmptyEntries);
+            textArray = point.Select(a => a.Split(',')).ToList();
+            return textArray.All(a => a.Length == 3);
+        }
+
+        public override void Execute()
+        {
+            OutPut = textArray
+                .Select(a => new TextSegment(a[0], double.Parse(a[1]), double.Parse(a[1])))
+                .ToArray();
+        }
+    }
+
+
+    public struct TextSegment
+    {
+        public string Text { set; get; }
+        public Point Point { set; get; }
+
+        public TextSegment(string text, double x, double y)
+        {
+            Text = text;
+            Point = new Point(x, y);
+        }
+    }
+}
