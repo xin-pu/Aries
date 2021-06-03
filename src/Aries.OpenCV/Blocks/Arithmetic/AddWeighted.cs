ac30b1a46f2dad9d8d0271b7f26a5fb692cf37d1
xin-pu <pu.xin@outlook.com>
2021-06-03 10:17:45 +0800

    Add AddWeighted Block

--- src/Aries.OpenCV/Blocks/Arithmetic/AddWeighted.cs
+++ src/Aries.OpenCV/Blocks/Arithmetic/AddWeighted.cs
@@ -1,6 +1,25 @@
-﻿namespace Aries.OpenCV.Blocks
+﻿using System.ComponentModel;
+using OpenCvSharp;
+
+namespace Aries.OpenCV.Blocks
 {
-    public class AddWeighted
+    [Category("Arithmetic")]
+    public class AddWeighted : ArithmeticBasic
     {
+
+        [Category("Enter")] public double Alpha { set; get; } = 0.5;
+
+        [Category("Enter")] public double Beta { set; get; } = 0.5;
+
+        [Category("Enter")] public double Gamma { set; get; } = 0;
+
+        /// <summary>
+        /// dst = alpha cdot img1 + beta cdot img2 + gamma
+        /// </summary>
+        public override void Execute()
+        {
+            Output = new Mat();
+            Cv2.AddWeighted(InPut1, Alpha, InPut2, Beta, Gamma, Output);
+        }
     }
 }
