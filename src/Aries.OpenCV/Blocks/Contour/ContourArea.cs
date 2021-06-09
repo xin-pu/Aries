f0c5c07099510cd2150a6245a490254b06c8f6d1
xin-pu <pu.xin@outlook.com>
2021-06-09 16:33:20 +0800

    Add Contour Area and ContourPermeter

---
+++ src/Aries.OpenCV/Blocks/Contour/ContourArea.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using Aries.OpenCV.GraphModel;
+using OpenCvSharp;
+
+namespace Aries.OpenCV.Blocks
+{
+    [Category("Contour")]
+    public class ContourArea : ExportBlock<double>
+    {
+        [Category("Enter")] public bool Oriented { set; get; }
+
+        public override bool CanExecute()
+        {
+            return InPutMat != null;
+        }
+
+        public override void Execute()
+        {
+            ExportResult = Cv2.ContourArea(InPutMat, Oriented);
+        }
+    }
+}
---
+++ src/Aries.OpenCV/Blocks/Contour/ContourPermeter.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using Aries.OpenCV.GraphModel;
+using OpenCvSharp;
+
+namespace Aries.OpenCV.Blocks
+{
+    [Category("Contour")]
+    public class ContourPermeter : ExportBlock<double>
+    {
+        [Category("Enter")] public bool Closed { set; get; }
+
+        public override bool CanExecute()
+        {
+            return InPutMat != null;
+        }
+
+        public override void Execute()
+        {
+            ExportResult = Cv2.ArcLength(InPutMat, Closed);
+        }
+    }
+}
