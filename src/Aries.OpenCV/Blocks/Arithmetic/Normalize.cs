e4e8c2f2c35f2b740ea5adf0d05fd02c01f072f5
xin-pu <pu.xin@outlook.com>
2021-06-10 17:26:51 +0800

    Add Corner Function.

---
+++ src/Aries.OpenCV/Blocks/Arithmetic/Normalize.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Aries.OpenCV.GraphModel;
+
+namespace Aries.OpenCV.Blocks
+{
+    public class Normalize:MatProcessingBlock
+    {
+        public override bool CanExecute()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Aries.OpenCV/Blocks/Corner/CornerHarris.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using Aries.OpenCV.GraphModel;
+using OpenCvSharp;
+
+namespace Aries.OpenCV.Blocks
+{
+    [Category("Corner")]
+    public class CornerHarris : MatProcessingBlock
+    {
+
+        [Category("ARGUMENT")] public int BlockSize { set; get; } = 2;
+        [Category("ARGUMENT")] public int KSize { set; get; } = 3;
+        [Category("ARGUMENT")] public double K { set; get; } = 0.04;
+        [Category("ARGUMENT")] public BorderTypes BorderType { set; get; } = BorderTypes.Default;
+
+
+        public override bool CanExecute()
+        {
+            return InPutMat != null;
+        }
+
+
+
+        public override void Execute()
+        {
+            OutPutMat = new Mat();
+            Cv2.CornerHarris(InPutMat, OutPutMat, BlockSize, KSize, K, BorderType);
+        }
+
+
+
+    }
+}
