c0a59daa39a749e22e3a28993b9475a48470b810
xin-pu <pu.xin@outlook.com>
2021-12-02 14:12:05 +0800

    Add Contrast.

---
+++ src/Aries.OpenCV/BlockMat/Effects/Contrast.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using Aries.OpenCV.GraphModel;
+using OpenCvSharp;
+
+namespace Aries.OpenCV.BlockMat.Effects
+{
+    /// <summary>
+    ///     对比度增强
+    /// </summary>
+    public class Contrast : MatProcess
+    {
+        [Category("ARGUMENT")] public double Alpha { set; get; } = 1.2;
+
+        [Category("ARGUMENT")] public double Beta { set; get; } = 30;
+
+        public override bool CanCall()
+        {
+            return MatIn != null;
+        }
+
+        public override void Call()
+        {
+            MatOut = new Mat();
+
+
+            MatOut = MatIn.Multiply(Alpha).Add(Beta);
+        }
+    }
+}
